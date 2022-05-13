e273d084d6a85f66d182e77ba408370d9ebdd549
pbrianandj <bcj26@case.edu>
2022-05-12 18:48:23 -0700

    it works

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,12 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import Content from "./components/Content";
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="h-screen w-screen bg-slate-800 p-5">
+      <Content />
     </div>
   );
-}
+};
 
 export default App;
---
+++ src/components/Content/index.tsx
@@ -0,0 +1,67 @@
+import { useCoingeckoPrice } from "@usedapp/coingecko";
+import { useEffect, useState } from "react";
+
+const Content = () => {
+    const [ethPrice, setEthPrice] = useState(0);
+    const [lastFetchTimestamp, setLastFetchTimestamp] = useState(0);
+    const [queryString,setQueryString] = useState("");
+    const [isLoading, setIsLoading] = useState(false);
+    const [imageURL, setImageURL] = useState("");
+
+    useEffect(() => {
+        const fetchData = async () => {
+            fetch("https://min-api.cryptocompare.com/data/price?fsym=ETH&tsyms=USD")
+            .then((data) => data.json())
+            .then((data) => {
+                setEthPrice(Math.round(Number(data.USD)));
+                setQueryString(
+                    encodeURIComponent(
+                    `The year ${Math.round(Number(data.USD))}`));
+                setLastFetchTimestamp(Date.now()/1000);
+            })
+            .catch((err) => console.log(err))
+        };
+
+        if ((Date.now()/1000  - lastFetchTimestamp) > 10) {
+            fetchData();
+        };
+    },[lastFetchTimestamp]);
+
+    useEffect(() => {
+        const fetchData = async () => {
+            fetch(`https://customsearch.googleapis.com/customsearch/v1?cx=${process.env.REACT_APP_CUSTOM_SEARCH_ENGINE_ID}&searchType=image&key=${process.env.REACT_APP_GOOGLE_IMG_API_KEY}&q=${queryString}&num=1`)
+            .then((data) => data.json())
+            .then((data) => {
+                setImageURL(data.items[0].link);
+            })
+            .catch((err) => console.log(err))
+        };
+
+        if (!isLoading && queryString.length > 0) {
+            setIsLoading(true);
+            fetchData();
+        };
+    }, [queryString]);
+
+    return (
+        <>
+        
+         <div className="mx-auto w-fit text-xl text-white font-bold opacity-50">
+             ONE IMAGE. EVERY TICK. UNTIL MY API KEY RUNS OUT.
+         </div>
+
+         <div className="mx-auto w-fit text-3xl text-white font-bold mt-6">
+                ETH Price: ${ethPrice}
+         </div>
+
+         <div className="mx-auto w-fit text-xl text-white font-bold mt-9">
+             <span className="font-medium opacity-50">Top Google image result for: </span> "The Year {ethPrice}"
+         </div>
+
+            <img src={imageURL} className="mx-auto mt-6"/>
+
+        </>
+    );
+};
+
+export default Content;
